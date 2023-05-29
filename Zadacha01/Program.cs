59c93d9f16c23e7aaa08d572810214d1eb3648d3
artem <artem28282828@gmail.com>
2023-05-29 17:17:00 +0500

    Первая задача

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+var StrNumber = Console.ReadLine();
+int number;
+bool isNumber = int.TryParse(StrNumber, out number);
+
+if (isNumber &&  StrNumber.Length == 5)
+{
+Console.WriteLine("Успешно! В вашем запросе только цифры.");
+    if(StrNumber[0] == StrNumber[4] && StrNumber[1] == StrNumber[3])
+    {
+        Console.WriteLine($"Число {number} является палиндромом ");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    } 
+}
+else
+{
+Console.WriteLine("Ошибка!!!!!");
+
+}
+
+
+
